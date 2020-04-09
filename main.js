4d46cd6173b81b4441a56d867bc5dcce67405b83
abidurr <42abid@gmail.com>
2020-04-09 03:56:26 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,138 @@
+FEB
+14
+2020
+Brussels, Belgium
+Le Lac
+
+NOV
+28
+2019
+Etterbeek, Belgium
+Atelier 210
+
+SEP
+28
+2019
+Orleans, France
+L'ASTROLABE
+
+JUL
+05
+2019
+Bailleul-Sir-Berthoult, France
+En Nord Beat Festival
+
+JUN
+28
+2019
+Andenne, Belgium
+Bear Rock Festival
+
+JUN
+01
+2019
+Tubize, Belgium
+L'Antistatic Zone
+
+MAY
+22
+2019
+Walloon Brabant, Belgium
+Farm Biéreau
+
+MAY
+11
+2019
+Gent, Belgium
+De Kleine Kunst
+
+APR
+26
+2019
+Quievrain, Belgium
+Floralies Festival
+
+APR
+25
+2019
+Saint-Josse-ten-Noode, Belgium
+Les Nuits Botanique
+
+APR
+20
+2019
+Leuze-en-Hainaut, Belgium
+Rock n' Trolls
+
+APR
+17
+2019
+Giessen, Germany
+AK44
+
+APR
+13
+2019
+Beauraing, Belgium
+BIND Festival
+
+MAR
+22
+2019
+Silly, Belgium
+Le Salon
+
+MAR
+16
+2019
+Lille, France
+Do It Yourself Café
+
+MAR
+07
+2019
+Tours, France
+Le Campus
+
+MAR
+05
+2019
+Angers, France
+Joker's Pub
+
+MAR
+04
+2019
+Saintes, France
+Dans l'Œil du Silo
+
+MAR
+03
+2019
+Tulle, France
+Le Conseil Général
+
+MAR
+02
+2019
+Angoulême, France
+Le Mars
+
+MAR
+01
+2019
+Paris, France
+SUPERSONIC
+
+JAN
+26
+2019
+Tournai, Belgium
+Maison de la Culture de Tournai
+
+JAN
+18
+2019
+Namur, Belgium
+Piano Bar Namur
+I Was There
